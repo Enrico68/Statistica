ef928a3c3f8243020d9b87f908b79f4700ce45a9
Enrico Pirani <enrico.pirani@gmail.com>
2025-06-20 23:36:28 +0200

    pulizia

--- crypto_quote.R
+++
@@ -1,9 +0,0 @@
-## BTC OHLC prices
-## from Binance spot market
-## in 30 minute intervals
-BTC_LS <- cryptoQuotes::get_lsratio(
-  ticker   = 'PI_ETHUSD',
-  source   = 'kraken',
-  interval = '2d',
-  from     = Sys.Date() - 500
-)
